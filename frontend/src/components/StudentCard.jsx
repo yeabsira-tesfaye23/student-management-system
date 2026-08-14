ad745e554e7a3989f85ad3633b314ac78c94a0da
Yeabsira <yeabnegn11@gmail.com>
2026-08-14 20:32:58 +0300

    update

--- frontend/src/components/StudentCard.jsx
+++ frontend/src/components/StudentCard.jsx
@@ -24,7 +24,7 @@ function StudentCard({ student, handleEdit, handleDelete }) {
                     className="edit-btn"
                     onClick={() => handleEdit(student)}
                 >
-                    Edit
+                    Edit this card please🥹
                 </button>
 
                 <button
