a2d3a1e5bcb5f893f71ea272144896b143bd0da9
Yeabsira <yeabnegn11@gmail.com>
2026-08-17 09:58:06 +0300

    updated

--- backend/sequelize.js
+++ backend/sequelize.js
@@ -9,7 +9,16 @@ const sequelize = new Sequelize(
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialect: "postgres",
+        logging: console.log
     }
 );
+
+sequelize.authenticate()
+    .then(() => {
+        console.log("PostgreSQL connected through Sequelize!");
+    })
+    .catch((error) => {
+        console.error("Unable to connect to PostgreSQL:", error);
+    });
 
 module.exports = sequelize;
