6ba26aa2227f094b940f4518961ef8eb23789f86
Yeabsira <yeabnegn11@gmail.com>
2026-08-18 21:34:22 +0300

    update

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../sequelize");
+
+const User = sequelize.define("User", {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "student"
+    }
+}, {
+    tableName: "users",
+    timestamps: true
+});
+
+module.exports = User;
